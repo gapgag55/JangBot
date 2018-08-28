24e55af795f562328fc634c141f245c85eecd903
gapgag55 <imkopkap@gmail.com>
2018-08-29 00:47:45 +0700

    update

--- Server/src/api/homework/homework.model.js
+++ Server/src/api/homework/homework.model.js
@@ -3,8 +3,9 @@ import mongoose, { Schema } from 'mongoose';
 const homeworkSchema = new Schema({
   subject: { type: String, required: true },
   description: { type: String, required: true },
+  type: { type: String, required: true },
   due_date: { type: Date, required: true },
-  notify_day: { type: Number, required: true },
+  appointment: { type: Number, required: true },
 });
 
 export default mongoose.model('Homework', homeworkSchema);
